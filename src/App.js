c4c46fac18552a0fd9bd84737457de6f8f389444
Parinphat Wasinchaikulpat <parinphatw@gmail.com>
2021-07-23 23:08:32 +0700

    update packages & styling

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import "./App.css";
 import RouteApp from "./Pages";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title =
+      "Skooldio (สคูลดิโอ) | คอร์สเรียนออนไลน์และเวิร์กชอปพัฒนาทักษะ";
+  }, []);
   return (
     <>
       <Navbar />
@@ -13,8 +18,5 @@ function App() {
     </>
   );
 }
-
-document.title =
-  "Skooldio (สคูลดิโอ) | คอร์สเรียนออนไลน์และเวิร์กชอปพัฒนาทักษะ";
 
 export default App;
